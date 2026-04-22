71de1f5b5c3a01de64164f05e20b1d6784bcd948
gsaraiva2109 <gsaraiva2109@gmail.com>
2026-04-22 13:54:36 -0300

    fix(ci): replace jq with node in sr-bump-version.sh to avoid missing dependency

--- scripts/sr-bump-version.sh
+++ scripts/sr-bump-version.sh
@@ -8,15 +8,23 @@ echo "$NEW" > "$ROOT/VERSION"
 sed -i "0,/^version = \"[0-9]*\.[0-9]*\.[0-9]*\"/{s/^version = \"[0-9]*\.[0-9]*\.[0-9]*\"/version = \"${NEW}\"/}" \
   "$ROOT/vinyl-catalog/src-tauri/Cargo.toml"
 
-tmp="$(mktemp)"
-jq --arg v "$NEW" '.version = $v' "$ROOT/vinyl-catalog/src-tauri/tauri.conf.json" > "$tmp"
-mv "$tmp" "$ROOT/vinyl-catalog/src-tauri/tauri.conf.json"
+node -e "
+  const fs = require('fs');
+  const f = '$ROOT/vinyl-catalog/src-tauri/tauri.conf.json';
+  const obj = JSON.parse(fs.readFileSync(f, 'utf8'));
+  obj.version = '$NEW';
+  fs.writeFileSync(f, JSON.stringify(obj, null, 2) + '\n');
+"
 
 sed -i "s/version: '[0-9]\+\.[0-9]\+\.[0-9]\+'/version: '${NEW}'/" \
   "$ROOT/vinyl-catalog/backend/src/swagger.ts"
 
 for pkg in vinylRecognizerDashboard/package.json vinyl-catalog/package.json vinyl-catalog/backend/package.json; do
-  tmp="$(mktemp)"
-  jq --arg v "$NEW" '.version = $v' "$ROOT/$pkg" > "$tmp"
-  mv "$tmp" "$ROOT/$pkg"
+  node -e "
+    const fs = require('fs');
+    const f = '$ROOT/$pkg';
+    const obj = JSON.parse(fs.readFileSync(f, 'utf8'));
+    obj.version = '$NEW';
+    fs.writeFileSync(f, JSON.stringify(obj, null, 2) + '\n');
+  "
 done
