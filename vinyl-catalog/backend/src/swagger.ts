36f3dd6abe5ba0258d435222afd0f2d6ad36bc9e
gsaraiva2109 <gsaraiva2109@gmail.com>
2026-04-22 17:27:50 +0000

    chore: release v2.2.3 [skip ci]

--- vinyl-catalog/backend/src/swagger.ts
+++ vinyl-catalog/backend/src/swagger.ts
@@ -7,7 +7,7 @@ const options = {
     openapi: '3.0.0',
     info: {
       title: 'Vinyl Vault API',
-      version: '2.2.2',
+      version: '2.2.3',
       description: 'API documentation for Vinyl Vault',
     },
     components: {
