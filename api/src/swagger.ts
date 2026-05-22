b615327abdb3c44c6de2b02f75d7253c238e8507
gsaraiva2109 <gsaraiva2109@gmail.com>
2026-05-22 18:22:12 +0000

    chore: release v1.3.2 [skip ci]

--- api/src/swagger.ts
+++ api/src/swagger.ts
@@ -7,7 +7,7 @@ const options = {
     openapi: '3.0.0',
     info: {
       title: 'Vinyl Vault API',
-      version: '1.3.1',
+      version: '1.3.2',
       description: 'API documentation for Vinyl Vault',
     },
     components: {
